e474e57025e165976412b917dbe6f78f1f76a0c6
rury fatkhuri fazri <fatkhurifazri@gmail.com>
2020-10-18 21:03:19 +0700

    uploading complete project

---
+++ therestaurant/src/components/Hero.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hero">
+      
+    <!-- dekstop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicius Food Menu, </strong> <br />in your gadget
+              </h2>
+              <p>pilih dan pesan makanan favorit anda</p>
+              <router-link class="btn btn-success" to="/foods">
+              <b-icon-arrow-right></b-icon-arrow-right>Pesan</router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/images/hero.png" width="100%" alt="" />
+        </div>
+      </div>
+    </div>
+
+
+    <!-- mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6 mb-3">
+          <img src="../assets/images/hero.png" width="100%" alt="" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicius Food Menu, </strong> <br />in your gadget
+              </h2>
+              <p>pilih dan pesan makanan favorit anda</p>
+              <button class="btn btn-success">Pesan</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hero",
+};
+</script>
+
+<style>
+</style>
